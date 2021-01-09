84a7e2e413f8c16694f055ac0ad399859da1a472
Eddie Joseph <eddie.joseph@windowslive.com>
2021-01-09 17:47:07 +0100

    removed unused code and comments.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -10,61 +10,20 @@ import network from './network'
 
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 
-//const ch = network.credHolder;
-
-
-//network.credHolder.authenticate()
-/*while(!ch.authenticated()){
-
-}*/
-console.log(network.credHolder.checkToken())
-//network.credHolder.authenticated()
-
-//network.credHolder.checkToken()
-console.log(network.checkToken())
-
-/*while(!network.credHolder.checkToken()){
-
-}*/
-/*
-if(network.credHolder.authenticated()){
-//if(ch.authenticated()){
-    //network.credHolder.getResource()
-
+network.authenticated(()=>{
     var callback = (resp) =>{console.log(resp)}
-    //console.log(ch.getResource("/block",callback))
-    console.log(network.credHolder.getResource("/block",callback))
+    console.log(network.getResource("/block",callback))
 
-ReactDOM.render(
+    ReactDOM.render(
     <Provider store={store}>
         <App />
     </Provider>, 
     document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-}*/
-
-
-//network.credHolder.authenticated(()=>{
-network.authenticated(()=>{
-
-var callback = (resp) =>{console.log(resp)}
-//console.log(network.credHolder.getResource("/block",callback))
-console.log(network.getResource("/block",callback))
-
-ReactDOM.render(
-<Provider store={store}>
-    <App />
-</Provider>, 
-document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    // If you want your app to work offline and load faster, you can change
+    // unregister() to register() below. Note this comes with some pitfalls.
+    // Learn more about service workers: https://bit.ly/CRA-PWA
+    serviceWorker.unregister();
 })
 
 
